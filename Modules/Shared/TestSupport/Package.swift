fb4ba77683c0b99c7ce1cdae9934a774b063107f
Anthony Tran <qquang269@gmail.com>
2026-09-07 19:04:45 +0700

    Add TestSupport package with Point-Free stack

---
+++ Modules/Shared/TestSupport/Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "TestSupport",
+    platforms: [.iOS(.v18), .macOS(.v15)],
+    products: [.library(name: "TestSupport", targets: ["TestSupport"])],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.3.2"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.0"),
+        .package(url: "https://github.com/pointfreeco/swift-clocks", from: "1.0.6"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.9.0"),
+    ],
+    targets: [
+        .target(
+            name: "TestSupport",
+            dependencies: [
+                .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras"),
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+                .product(name: "Clocks", package: "swift-clocks"),
+                .product(name: "DependenciesTestSupport", package: "swift-dependencies"),
+            ]
+        ),
+        .testTarget(
+            name: "TestSupportTests",
+            dependencies: ["TestSupport"],
+            resources: [.copy("Fixtures")]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
